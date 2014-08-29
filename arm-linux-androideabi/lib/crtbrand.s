3ad6e598f88a209fe07eb6923379ccb2914c25a5
JustArchi <JustArchi@JustArchi.net>
2014-08-29 07:39:26 +0200

    Daily 29 August

---
+++ arm-linux-androideabi/lib/crtbrand.s
@@ -0,0 +1,24 @@
+	.arch armv5te
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"crtbrand.c"
+	.section	.note.android.ident,"a",%progbits
+	.align	2
+	.type	abitag, %object
+	.size	abitag, 24
+abitag:
+	.word	8
+	.word	4
+	.word	1
+	.ascii	"Android\000"
+	.word	19
+	.ident	"GCC: (Linaro GCC 4.9-2014.08-1~dev) 4.9.2 20140811 (prerelease)"
+	.section	.note.GNU-stack,"",%progbits
