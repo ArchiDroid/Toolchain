97d2e7c8094fa3287b838789a55dcdba934fb714
JustArchi <JustArchi@JustArchi.net>
2015-07-29 10:43:26 +0200

    2015-05-29

---
+++ include/isl/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.14"
+/* generated using gnu compiler x86_64-linux-gnu-gcc (Ubuntu 5.1.1-4ubuntu12) 5.1.1 20150504 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
